503e940a3af731b713324ac754501b38474368b0
nghitrum <nguyenvietnghi@live.com>
2020-06-16 23:58:01 +0300

    Improve performance for Modal

--- src/components/Burger/OrderSummary/index.js
+++ src/components/Burger/OrderSummary/index.js
@@ -1,36 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Button from '../../UI/Button';
 
-const OrderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients).map(
-		(ingredientKey) => {
-			return (
-				<li key={ingredientKey}>
-					<span style={{ textTransform: 'capitalize' }}>
-						{ingredientKey}
-					</span>
-					: {props.ingredients[ingredientKey]}
-				</li>
-			);
-		}
-	);
-	return (
-		<Fragment>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>{ingredientSummary}</ul>
-			<p>
-				<strong>Total prices: €{props.price.toFixed(2)}</strong>
-			</p>
-			<p>Continue to Checkout?</p>
-			<Button btnType={'Danger'} clicked={props.purchaseCanceled}>
-				CANCEL
-			</Button>
-			<Button btnType={'Success'} clicked={props.purchaseContinued}>
-				CONTINUE
-			</Button>
-		</Fragment>
-	);
-};
+class OrderSummary extends Component {
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients).map(
+			(ingredientKey) => {
+				return (
+					<li key={ingredientKey}>
+						<span style={{ textTransform: 'capitalize' }}>
+							{ingredientKey}
+						</span>
+						: {this.props.ingredients[ingredientKey]}
+					</li>
+				);
+			}
+		);
+		return (
+			<Fragment>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>{ingredientSummary}</ul>
+				<p>
+					<strong>
+						Total prices: €{this.props.price.toFixed(2)}
+					</strong>
+				</p>
+				<p>Continue to Checkout?</p>
+				<Button
+					btnType={'Danger'}
+					clicked={this.props.purchaseCanceled}
+				>
+					CANCEL
+				</Button>
+				<Button
+					btnType={'Success'}
+					clicked={this.props.purchaseContinued}
+				>
+					CONTINUE
+				</Button>
+			</Fragment>
+		);
+	}
+}
 
 export default OrderSummary;
